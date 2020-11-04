a312ade54f185bf168a933ee61f0130cdadfe11a
ItzikMilshtein <itzikmilsh@gmail.com>
2020-11-04 20:40:21 +0200

    Itzik - Added comments

--- Demo-toRGB.py
+++ Demo-toRGB.py
@@ -8,11 +8,11 @@ def main():
     If the files are already RGB, the functions does nothing.
     """
     ball_files, bg_files = os.listdir("Demo-BallSet"), os.listdir("Demo-BGSet")
-    for ball in (ball_files):
-        rgb_ball_image = Image.open("Demo-BallSet" + os.sep + ball).convert('RGB')
-        np_ball = np.array(rgb_ball_image)
-        Image.fromarray(np_ball).save("Demo-BallSet" + os.sep + ball)
-    for bg in (bg_files):
+    for ball in (ball_files): #for each ball in the files
+        rgb_ball_image = Image.open("Demo-BallSet" + os.sep + ball).convert('RGB') #set it to RGB
+        np_ball = np.array(rgb_ball_image) #turm image to np.ndarray
+        Image.fromarray(np_ball).save("Demo-BallSet" + os.sep + ball) #save it in the same file it was before
+    for bg in (bg_files): #same for backgrounds...
         rgb_bg_image = Image.open("Demo-BGSet" + os.sep + bg).convert('RGB')
         np_bg = np.array(rgb_bg_image)
         Image.fromarray(np_bg).save("Demo-BGSet" + os.sep + bg)
